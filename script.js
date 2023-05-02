21af1deb8942b48f3ac4b2d64e409a21f22c7975
sujeong6199 <117880507+sujeong6199@users.noreply.github.com>
2023-05-02 20:35:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+let menuMobile = document.querySelector("#menu_mobile");
+let menuIcon = document.querySelector("#menu_icon");
+let menuIconX = document.querySelector("#menu_icon_x");
+
+menuIcon.addEventListener("click",()=>{
+    menuMobile.style.visibility = "visible";
+    menuMobile.style.opacity = 1;
+});
+
+menuIconX.addEventListener("click",()=>{
+    menuMobile.style.visibility = "hidden";
+    menuMobile.style.opacity = 0;
+});
